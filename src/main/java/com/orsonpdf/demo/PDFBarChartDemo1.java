a2f17ae8cbcc8f7e5a9fdb4597953a56218c45dc
David Gilbert <david.gilbert@object-refinery.com>
2013-09-03 11:03:36 +0200

    Demo programs.

---
+++ src/main/java/com/orsonpdf/demo/PDFBarChartDemo1.java
@@ -0,0 +1,122 @@
+/* =====================================================================
+ * OrsonPDF : a fast, light-weight PDF library for the Java(tm) platform
+ * =====================================================================
+ * 
+ * (C)opyright 2013, by Object Refinery Limited.  All rights reserved.
+ *
+ * Project Info:  http://www.object-refinery.com/pdf/index.html
+ */
+
+package com.orsonpdf.demo;
+
+import com.orsonpdf.PDFDocument;
+import com.orsonpdf.PDFGraphics2D;
+import com.orsonpdf.Page;
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Rectangle;
+import java.io.File;
+import java.io.IOException;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.labels.ItemLabelAnchor;
+import org.jfree.chart.labels.ItemLabelPosition;
+import org.jfree.chart.labels.StandardCategoryItemLabelGenerator;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.renderer.category.StatisticalBarRenderer;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.statistics.DefaultStatisticalCategoryDataset;
+import org.jfree.ui.TextAnchor;
+
+/**
+ * A demo for PDF output of a bar chart.
+ */
+public class PDFBarChartDemo1 {
+
+    /**
+     * Creates a sample dataset.
+     *
+     * @return The dataset.
+     */
+    private static CategoryDataset createDataset() {
+        DefaultStatisticalCategoryDataset dataset
+            = new DefaultStatisticalCategoryDataset();
+        dataset.add(10.0, 2.4, "Row 1", "Column 1");
+        dataset.add(15.0, 4.4, "Row 1", "Column 2");
+        dataset.add(13.0, 2.1, "Row 1", "Column 3");
+        dataset.add(7.0, 1.3, "Row 1", "Column 4");
+        dataset.add(22.0, 2.4, "Row 2", "Column 1");
+        dataset.add(18.0, 4.4, "Row 2", "Column 2");
+        dataset.add(28.0, 2.1, "Row 2", "Column 3");
+        dataset.add(17.0, 1.3, "Row 2", "Column 4");
+        return dataset;
+    }
+
+    /**
+     * Creates a sample chart.
+     *
+     * @param dataset  a dataset.
+     *
+     * @return The chart.
+     */
+    private static JFreeChart createChart(CategoryDataset dataset) {
+
+        // create the chart...
+        JFreeChart chart = ChartFactory.createLineChart(
+            "Statistical Bar Chart Demo 1", "Type", "Value", dataset);
+
+        CategoryPlot plot = (CategoryPlot) chart.getPlot();
+
+        // customise the range axis...
+        NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+        rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+        rangeAxis.setAutoRangeIncludesZero(false);
+
+        // customise the renderer...
+        StatisticalBarRenderer renderer = new StatisticalBarRenderer();
+        renderer.setDrawBarOutline(false);
+        renderer.setErrorIndicatorPaint(Color.black);
+        renderer.setIncludeBaseInRange(false);
+        plot.setRenderer(renderer);
+
+        // ensure the current theme is applied to the renderer just added
+        ChartUtilities.applyCurrentTheme(chart);
+
+        renderer.setBaseItemLabelGenerator(
+                new StandardCategoryItemLabelGenerator());
+        renderer.setBaseItemLabelsVisible(true);
+        renderer.setBaseItemLabelPaint(Color.yellow);
+        renderer.setBasePositiveItemLabelPosition(new ItemLabelPosition(
+                ItemLabelAnchor.INSIDE6, TextAnchor.BOTTOM_CENTER));
+
+        // set up gradient paints for series...
+        GradientPaint gp0 = new GradientPaint(0.0f, 0.0f, Color.blue,
+                0.0f, 0.0f, new Color(0, 0, 64));
+        GradientPaint gp1 = new GradientPaint(0.0f, 0.0f, Color.green,
+                0.0f, 0.0f, new Color(0, 64, 0));
+        renderer.setSeriesPaint(0, gp0);
+        renderer.setSeriesPaint(1, gp1);
+        return chart;
+    }
+ 
+    /**
+     * Starting point for the demo.
+     * 
+     * @param args  ignored.
+     * 
+     * @throws IOException 
+     */
+    public static void main(String[] args) throws IOException {
+        JFreeChart chart = createChart(createDataset());
+        PDFDocument pdfDoc = new PDFDocument();
+        pdfDoc.setTitle("PDFBarChartDemo1");
+        pdfDoc.setAuthor("Object Refinery Limited");
+        Page page = pdfDoc.createPage(new Rectangle(612, 468));
+        PDFGraphics2D g2 = page.getGraphics2D();
+        chart.draw(g2, new Rectangle(0, 0, 612, 468));
+        pdfDoc.writeToFile(new File("PDFBarChartDemo1.pdf"));
+    }
+
+}

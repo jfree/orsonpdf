1705041f30d9be8ff4762bbf5577e494d7e1965f
David Gilbert <dave@jfree.org>
2015-09-18 19:00:04 +0200

    Correction for header

--- src/main/java/com/orsonpdf/PDFHints.java
+++ src/main/java/com/orsonpdf/PDFHints.java
@@ -6,6 +6,28 @@
  *
  * Project Info:  http://www.object-refinery.com/orsonpdf/index.html
  * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
+ * Other names may be trademarks of their respective owners.]
+ * 
+ * If you do not wish to be bound by the terms of the GPL, an alternative
+ * commercial license can be purchased.  For details, please see visit the
+ * Orson PDF home page:
+ * 
+ * http://www.object-refinery.com/orsonpdf/index.html
+ * 
  */
 
 package com.orsonpdf;

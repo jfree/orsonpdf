0a442e8325fcd94e912d8b0f060ddf283a652e6b
qwert2003 <qwert2003@users.sourceforge.net>
2018-12-02 22:53:18 +0100

    Allow calling drawString with empty string.

--- src/main/java/com/orsonpdf/PDFGraphics2D.java
+++ src/main/java/com/orsonpdf/PDFGraphics2D.java
@@ -623,9 +623,11 @@ public final class PDFGraphics2D extends Graphics2D {
                 this.hints.get(PDFHints.KEY_DRAW_STRING_TYPE))) {
             this.gs.drawString(str, x, y);
         } else {
-            AttributedString as = new AttributedString(str, 
-                    this.font.getAttributes());
-            drawString(as.getIterator(), x, y);
+            if (str.length() > 0) { // avoid IllegalArgumentException for empty string
+                AttributedString as = new AttributedString(str, 
+                        this.font.getAttributes());
+                drawString(as.getIterator(), x, y);
+            }
         }
         
         if (this.clip != null) {
